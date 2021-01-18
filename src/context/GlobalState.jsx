629596bf9e2b334aa9d0634f834e46a9b8a1cb62
Fuad Abdella <v-fabdella@microsoft.com>
2021-01-18 01:16:11 -0800

    updated code

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer'
 // Initial State
 
 const initialState = {
-    transactions: [
-      { id: 1, text: 'Flower', amount: -20 },
-      { id: 2, text: 'Salary', amount: 300 },
-      { id: 3, text: 'Book', amount: -10 },
-      { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions: []
 }
 
 
@@ -23,9 +18,33 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+// Actions ========================================================
+    const deleteTransaction = (id) => {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+
+
+
+    const addTransaction = (transaction) => {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+//==================================================================
+
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
 }
+
+
+
